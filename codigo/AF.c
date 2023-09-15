376fddc83ef1c90986f1b01d8393859915925719
I-Calo <129707170+I-Calo@users.noreply.github.com>
2023-09-15 17:20:24 -0300

    automatas imlpementados

--- codigo/AF.c
+++ codigo/AF.c
@@ -6,11 +6,17 @@
 int main(void){
     char * cadena="-1455464";
     char *octal="015$";
-    int d,o;
+    char *decimal = "-34352";
+    char *hexa = "0X75aF";
+    int d,o,e,h;
     d= caracterComoEntero(cadena);
     printf("%d\n",d);
     o = automataOctal(octal);// 0 si es aceptado, -1 si es rechazado
+    e = automataDecimal(decimal);
+    h = automataHexadecimal(hexa);
     printf("Resultado del Autómata Octal: %d", o);
+    printf("\nResultado del Autómata Decimal: %d", e);
+    printf("\nResultado del Autómata Hexadecimal: %d", h);
     return 0;
 }
 
@@ -55,11 +61,89 @@ int automataOctal(char *cadena){
     else return -1;
 }
 
+// Automata Decimal
+int automataDecimal(char* cadena){
 
+    //TT
+    size_t estados[][4] = {
+    //   -,     0,    [1-9],   $
+        {3,     1,      2,     4},//0
+        {4,     4,      4,     0},//1
+        {4,     2,      2,     0},//2
+        {4,     4,      2,     4},//3
+        {4,     4,      4,     4} //4
+    };
 
+    size_t estado_final = 0;
 
+    size_t estadoInicial = 0;
 
+    size_t estado = estadoInicial;
+    int i = 0;
+    char c;
+    int largo_cadena = strlen(cadena);
+    for(i=0;i<largo_cadena;i++){
+        //Caracter donde está parado
+        c=cadena[i];
+        //Hace la transición respecto a cuál caracter del alfabeto se está procesando
+        if(c=='-') estado = estados[estado][0];
+        else if(c == '0') estado = estados[estado] [1];
+        else if(c >= '1' && c <='9') estado = estados[estado][2];
+        else if(c == '$') estado = estados[estado][3];
+        //Si no pertenece al alfabeto va directo a estado de rechazo
+        else estado = 4;
 
+        //Si se rechaza, ya salimos del autómata y lo declaramos inválido
+        if(estado == 4) return 0;
+    }
+
+    //Si no acabó en un estado final, el autómata se rechaza por estar incompleto
+    if(estado == estado_final) return 1;
+    else return 0;
+
+}
+
+// Automata Hexadecimal
+int automataHexadecimal(char* cadena){
+
+    //TT
+    size_t estados[][4] = {
+    //   0,    [Xx],   [1-9A-Fa-f],   $
+        {1,     4,        4,          4},//0
+        {4,     2,        4,          4},//1
+        {4,     4,        3,          4},//2
+        {3,     4,        3,          0},//3
+        {4,     4,        4,          4} //4
+    };
+
+    size_t estado_final = 0;
+
+    size_t estadoInicial = 0;
+
+    size_t estado = estadoInicial;
+    int i = 0;
+    char c;
+    int largo_cadena = strlen(cadena);
+    for(i=0;i<largo_cadena;i++){
+        //Caracter donde está parado
+        c=cadena[i];
+        //Hace la transición respecto a cuál caracter del alfabeto se está procesando
+        if(c=='0') estado = estados[estado][0];
+        else if(c == 'X' || c== 'x') estado = estados[estado] [1];
+        else if(c >= '1' && c <='9' || c >= 'a' && c <='f' || c >= 'A' && c <='F') estado = estados[estado][2];
+        else if(c == '$') estado = estados[estado][3];
+        //Si no pertenece al alfabeto va directo a estado de rechazo
+        else estado = 4;
+
+        //Si se rechaza, ya salimos del autómata y lo declaramos inválido
+        if(estado == 4) return 0;
+    }
+
+    //Si no acabó en un estado final, el autómata se rechaza por estar incompleto
+    if(estado == estado_final) return 1;
+    else return 0;
+
+}
 
 
 
